914c423ddd0a1442f15a72377cdc2bd756f95607
Marco Bacer <bacer@esteco.com>
2016-12-05 10:02:32 +0100

    initial commit for android app

---
+++ android-app/README.md
@@ -0,0 +1,6 @@
+# camera2QRcodeReader
+Simple to use, real time QR code reader implementation for Android.
+Should give you an idea of how to use the new camera2 API for the application that run on Lollipop (API21) and up using the ImageReader's surface target - the camera streams the data through and you can check for a QR code in the real time.
+Hopefully it will be handy for some people.<br>
+NOTE: it is an experimental one and you should definitely use the idea ONLY and feel free to improve it and make pull requests.
+</br> Appreciate any advice or comment.
---
+++ android-app/app/src/main/java/com/qrcodereader/MainActivity.java
@@ -0,0 +1,52 @@
+package com.qrcodereader;
+
+import android.app.FragmentTransaction;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.RelativeLayout;
+
+/**
+ * MainActivity.class
+ * Created by Mihail on 10/21/15 <mihail@breadwallet.com>.
+ * Copyright (c) 2015 breadwallet llc.
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+public class MainActivity extends FragmentActivity {
+
+    public static final String TAG = MainActivity.class.getName();
+    public static Point screenParametersPoint = new Point();
+    public RelativeLayout layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindowManager().getDefaultDisplay().getSize(screenParametersPoint);
+        layout = (RelativeLayout) findViewById(R.id.main_layout);
+        if (savedInstanceState == null) {
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.replace(layout.getId(), new FragmentDecoder());
+            fragmentTransaction.commit();
+        }
+    }
+
+}
